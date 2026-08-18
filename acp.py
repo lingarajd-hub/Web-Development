77f649d2f6586c40af625ca813cab848ff30b5ad
lingarajd-hub <lingaraj.d@gmail.com>
2026-08-18 18:47:56 +1000

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,17 @@
+# Printing the title
+print("My Daily Routine")
+
+# Step 1: Waking Up
+print("I wake up at 7:00am.")
+
+# Step 2: Going to school
+print("I go to school at 8:00am.")
+
+# Step 3: Playing after school
+print("I play with my friends in the evening.")
+
+# Step 4: Studying
+print("I study for 2 hours at night.")
+
+# Calculating total study time in the week
+print("Total study hours in the week:", 2 * 7)
