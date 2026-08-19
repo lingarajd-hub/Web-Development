0adc45da623bc2ba49e3942d54a24f77d3403090
lingarajd-hub <lingaraj.d@gmail.com>
2026-08-19 18:53:36 +1000

    Add files via upload

--- acp.py
+++ acp.py
@@ -1,17 +1,11 @@
-# Printing the title
-print("My Daily Routine")
+import keyword
 
-# Step 1: Waking Up
-print("I wake up at 7:00am.")
+name = input("What is your name? ")
+goal = input("What is your personal goal? ")
+month = input("What month will you achieve it? ")
 
-# Step 2: Going to school
-print("I go to school at 8:00am.")
+print("\nPersonal Goals")
+print("Name:", name)
+print("Goal:", goal)
+print("Target Month:", month, end="\n\n")
 
-# Step 3: Playing after school
-print("I play with my friends in the evening.")
-
-# Step 4: Studying
-print("I study for 2 hours at night.")
-
-# Calculating total study time in the week
-print("Total study hours in the week:", 2 * 7)
